8cf3ec2363cd209a1bae0dfab0ac9d02157b3951
Andrew Azores <me@andrewazor.es>
2026-02-24 17:31:27 -0500

    resource limits

--- start-ollama.bash
+++ start-ollama.bash
@@ -4,7 +4,9 @@ PORT="${OLLAMA_PORT:-11434}"
 
 podman run --rm -it --replace \
     --pull always \
+    --name "${OLLAMA_CONTAINER_NAME:-ollama}" \
     -p "${PORT}:${PORT}" \
     -v "${OLLAMA_VOLUME_NAME:-ollama}":/root/.ollama \
-    --name "${OLLAMA_CONTAINER_NAME:-ollama}" \
+    --cpus "${OLLAMA_CPU_LIMIT:-8.0}" \
+    --memory "${OLLAMA_MEM_LIMIT:-16GiB}" \
     ollama
