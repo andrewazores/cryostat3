e41e958367fe5d46946d347129fd83dc1a0938e6
Andrew Azores <me@andrewazor.es>
2026-02-24 16:10:54 -0500

    add script

---
+++ start-ollama.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PORT="${OLLAMA_PORT}:-11434"
+
+podman run --rm -it --replace \
+    --pull always \
+    -p "${PORT}:${PORT}" \
+    -v "${OLLAMA_VOLUME_NAME:-ollama}":/root/.ollama \
+    --name "${OLLAMA_CONTAINER_NAME:-ollama}" \
+    ollama
